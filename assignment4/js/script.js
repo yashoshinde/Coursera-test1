2c49ca22ffd065a0d93be54c08528041ed604e84
yashoshinde <54619868+yashoshinde@users.noreply.github.com>
2020-05-30 11:20:48 +0530

    Create script.js

---
+++ assignment4/js/script.js
@@ -0,0 +1,15 @@
+function a()
+{
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (var i = 0; i < names.length; i++) {
+
+  
+  var firstLetter = names[i].charAt(0).toLowerCase();
+   if (firstLetter === 'j') {
+   document.write("Good bye "+ names[i]+"<br>");
+  } else {
+    document.write("Hello "+ names[i]+"<br>");
+  }
+}
+}
